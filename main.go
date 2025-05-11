556a35954fb5ab2cbc4d6d9faf25eb5ff4685a2b
Muhammad Arif Sulaksono <marifsulaksono@gmail.com>
2025-05-11 10:15:15 +0700

    feat: add endpoint about

--- main.go
+++ main.go
@@ -7,15 +7,21 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/v1/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.GET("/greeting", func(c *gin.Context) {
+	r.GET("/v1/greeting", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, World!",
+		})
+	})
+
+	r.GET("/v1/about", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Ini adlaah hands-on deployment VPS",
 		})
 	})
 
